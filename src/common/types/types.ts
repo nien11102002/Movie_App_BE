6ec142432ba35eab1affdf6c8a76c63bcae0c712
nien11102002 <nguyenduynien11102002@gmail.com>
2024-12-31 02:08:21 +0700

    set up common folder

---
+++ src/common/types/types.ts
@@ -0,0 +1,9 @@
+export type TUser = {
+  account_id?: number;
+  full_name?: string;
+  age?: number;
+  avatar?: string;
+  email?: string;
+  phone_number?: string;
+  user_type?: String;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { LoggingInterceptor } from './common/interceptor/logging.interceptor';
+import { ResponseSuccessInterceptor } from './common/interceptor/response-success.interceptor';
+import { AllExceptionFilter } from './common/filter/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const reflector = app.get(Reflector);
+
+  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalInterceptors(new ResponseSuccessInterceptor(reflector));
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS')
+    .setDescription('The NestJS API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
